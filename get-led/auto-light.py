f508dda5b3cc55e172d2a4cfbecd5b1c6ee58560
Артём Мальцев <adgj2005L@gmail.com>
2026-02-11 14:38:40 +0300

    script_4

--- get-led/auto-light.py
+++ get-led/auto-light.py
@@ -1,14 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-botton = 13
 led = 26
-GPIO.setup(botton, GPIO.IN)
 GPIO.setup(led, GPIO.OUT)
+p = 6
+GPIO.setup(p, GPIO.IN)
 state = 0
-
 while True:
-    if GPIO.input(botton):
-        state = not state
+    if GPIO.input(p):
+        state = 0
+        GPIO.output(led, state)
+        time.sleep(0.2)
+    else:
+        state = 1
         GPIO.output(led, state)
         time.sleep(0.2)
